cb40f9ad00ff325414c547fe8ef9a0564553e0b1
Brian <lbrian357@gmail.com>
2017-01-06 06:07:14 +0200

    score table feature added

--- controller.rb
+++ controller.rb
@@ -1,8 +1,40 @@
+require_relative 'mastermind.rb'
 require 'sinatra'
 require 'sinatra/reloader'
 
+class Counter
+  attr_accessor :count
+  def initialize
+    @count = 0
+  end
+
+  def up
+    @count += 1
+  end
+end
+
+def table(turn, guess, result)
+  return "<tr><td>#{turn}</td><td>#{guess}</td><td>#{result}</td></tr>"
+end
+
+a_game = Game.new
+count = Counter.new
+score = ''
+
 get '/' do
-  'Hello World'
+  # throw a_game.win.inspect
+  turn = count.count
+  count.up if turn == 0
+  turn = 1 if turn == 0
+  count = Counter.new if turn > 10 || a_game.win == true
+  a_game = Game.new if turn > 10 || a_game.win == true
+  score = '' if turn > 10 || a_game.win == true
+  count.up
+  guess = params['color_guess']
+  index_form_guess = color_to_index(guess.to_s)
+  result = a_game.guessing(a_game.picks, index_form_guess, turn)
+  score = score + table(turn, guess, result)
+  erb :index, :locals => {:a_game => a_game, :guess => guess, :result => result, :turn => turn, :score => score}
 end
 
 
--- mastermind.rb
+++ mastermind.rb
@@ -1,31 +1,28 @@
-module Generator
-  def c_code
-    colors = ['red','green','blue','yellow','brown','orange','black','white']
-    c = {}
-    colors.each {|i| c[(colors.index(i))] = i}
-    c
-  end
+def c_code
+  colors = ['red','green','blue','yellow','brown','orange','black','white']
+  c = {}
+  colors.each {|i| c[(colors.index(i))] = i}
+  c
+end
 
-  def pick
-    picks = []
-    4.times {picks << rand(0...8)}
-    picks
-  end
+def pick
+  picks = []
+  4.times {picks << rand(0...8)}
+  picks
+end
 
-  def color_to_index(string)
-    array = string.split(' ')
-    array.map { |i| c_code.invert[i] }
-  end
+def color_to_index(string)
+  array = string.split(' ')
+  array.map { |i| c_code.invert[i] }
+end
 
-  def index_to_color(array)
-    a = []
-    array.each { |i| a << c_code[i] }
-    a.join(' ')
-  end
+def index_to_color(array)
+  a = []
+  array.each { |i| a << c_code[i] }
+  a.join(' ')
 end
 
 class Game
-  include Generator
   attr_accessor :picks, :win
   def initialize
     #@picks = pick 
@@ -34,52 +31,11 @@ class Game
   end
 
   def start
-    puts 'would you like to solve my mastermind puzzle or would you like me to solve yours? Enter \'solve\' or \'pose\''
-    play_decision = gets.chomp
-    if play_decision == 'solve'
-      puts "Enter any of the following colors: #{c_code.values.join(', ')}"
-      puts "a 'O' signifies a correct color but an incorrect position, while a '@' signifies a correct color with the correct position as well"
-      puts "you have 10 turns"
-      human_guess
-    elsif play_decision == 'pose'
-      human_picks
-      comp_guess
-    else
-      puts 'I do not understand, please enter a valid response'
-      start
-    end
+    return "Enter any of the following colors: #{c_code.values.join(', ')}"
+    return "a 'O' signifies a correct color but an incorrect position, while a '@' signifies a correct color with the correct position as well"
+    return "you have 10 turns"
+    human_guess
   end
-
-  def human_picks
-    puts 'give me an four colors to crack human: '
-    h_p = gets.chomp
-    self.picks = color_to_index(h_p)
-  end
-
-  def comp_guess
-    keep = []
-    for i in 1..12
-      if win == true
-        break
-      end
-
-      
-      comp_guess = pick
-      keep.uniq.each do |k|
-        comp_guess.insert(rand(0..3), keep[k]).pop
-      end
-
-     guessing(comp_guess, i)
-
-      comp_guess.each do |j|
-        if picks.include?(j)
-          keep << j
-        end
-      end
-
- 
-    end
-  end 
 
   def human_guess
     #get user input on their guess
@@ -88,62 +44,57 @@ class Game
         break
       end
 
-      print "\n please enter your guess separated by a space: "
+      return "\nturn number #{i}"
+      return "please enter your guess separated by a space: "
       str_guess = gets.chomp 
       guess = color_to_index(str_guess) #turns user colors into corresponding color code number
-      guessing(guess, i) #gives color code to guessing function
+      guessing(picks,guess, i) #pass guess in color code to guessing function
     end
-    puts 'END'
+    return 'END'
   end
 
-  def guessing(guess, i)
-    puts "\n turn number #{i}"
+  def guessing(picks, guess, i) #compares answer with guess and outputs clue
     if guess == picks
-      self.win = true #find out WHY THE FUCK is attr_accessor not enough to change win = true to @win = true
-      
-      puts index_to_color(guess)
-      puts 'You Win!'
-    elsif i == 10
-      puts 'WRONG! You Lose!'
-      puts "the answer was #{index_to_color(picks)}"
-      
+      @win = true #find out WHY attr_accessor is not enough to change win = true to @win = true
 
+      #return index_to_color(guess)
+      return 'Correct! You Win!'
+    elsif i > 10
+      return "WRONG! You Lose!
+              \nthe answer was #{index_to_color(picks)}"
     else
       #check if user guess is included in answer
-      p index_to_color(guess)
+      #puts index_to_color(guess)
       #p guess
+
       correct_colors = []
-      picks.uniq.each do |i|
+
+      picks.uniq.each do |i| #loop computes number of correct colors
         if picks.count(i) <= guess.count(i) 
           picks.count(i).times {correct_colors << 'O'}
         else
           guess.count(i).times {correct_colors << 'O'}
         end
       end
-      #guess.uniq.each do |i|
-      #  picks.count(i).times {print 'O'}
-      #end
-      #guess.each { |i| print 'O' if picks.include?(i)}
-      for i in 0...4 do
+      for i in 0...4 do #loop computes number of correct positions
         if picks[i] == guess[i]
           correct_colors << '@'
           correct_colors.shift
         end
       end
-    end
-    puts correct_colors.join if win != true
+      return correct_colors.join if win != true && i != 10 #return clues
+      end
   end
 
   #print out clues if guesses were in answer
   #O for right color but wrong position
   #and @ for right color and right position
   #it should print how many chances are left and keep history of guesses
-
   #when guess == picks and guesses_left > 0 you win
 
 
 end
 
-a = Game.new
-a.start
+#a = Game.new
+#a.start
 
